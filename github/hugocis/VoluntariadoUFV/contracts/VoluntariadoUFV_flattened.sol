6d4ac17c8db19d7a2a64d89875235ef47b0a0853
JLDSjr <9103919@alumnos.ufv.es>
2024-05-20 00:52:46 +0200

    añadido extensión para burneable

--- github/hugocis/VoluntariadoUFV/contracts/VoluntariadoUFV_flattened.sol
+++ github/hugocis/VoluntariadoUFV/contracts/VoluntariadoUFV_flattened.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 // File: @openzeppelin/contracts/interfaces/draft-IERC6093.sol
 
 
@@ -1841,6 +1841,36 @@ abstract contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI, IER
     }
 }
 
+// File: @openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol
+
+
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC1155/extensions/ERC1155Burnable.sol)
+
+pragma solidity ^0.8.20;
+
+
+/**
+ * @dev Extension of {ERC1155} that allows token holders to destroy both their
+ * own tokens and those that they have been approved to use.
+ */
+abstract contract ERC1155Burnable is ERC1155 {
+    function burn(address account, uint256 id, uint256 value) public virtual {
+        if (account != _msgSender() && !isApprovedForAll(account, _msgSender())) {
+            revert ERC1155MissingApprovalForAll(_msgSender(), account);
+        }
+
+        _burn(account, id, value);
+    }
+
+    function burnBatch(address account, uint256[] memory ids, uint256[] memory values) public virtual {
+        if (account != _msgSender() && !isApprovedForAll(account, _msgSender())) {
+            revert ERC1155MissingApprovalForAll(_msgSender(), account);
+        }
+
+        _burnBatch(account, ids, values);
+    }
+}
+
 // File: @openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol
 
 
@@ -1886,6 +1916,7 @@ abstract contract ERC1155Pausable is ERC1155, Pausable {
 
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
+
 
 
 
